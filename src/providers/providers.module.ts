a08d5745101cc683c46cf3df39842654e70510ef
andres06-hub <andressierrarojas7@gmail.com>
2023-03-02 16:32:18 -0500

    ADD: provider

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule]
+  imports: [DatabaseModule],
+  exports: [DatabaseModule],
 })
 export class ProvidersModule {}
