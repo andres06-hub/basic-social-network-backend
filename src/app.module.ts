9362be141344e1768215de6885aa7f90eb6d5d09
andres06-hub <andressierrarojas7@gmail.com>
2023-03-02 18:25:34 -0500

    ADD: add modules, port and prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { ProvidersModule } from './providers/providers.module';
+import { ConfigService } from './config/config.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from './config/config.module';
+import { key } from './config/key.config';
 
 @Module({
-  imports: [ProvidersModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService],
+  imports: [ProvidersModule, AuthModule, UserModule, ConfigModule],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+  static prefix: string;
+
+  constructor(private readonly _configSrv: ConfigService) {
+    AppModule.port = this._configSrv.get(key.PORT);
+    AppModule.prefix = this._configSrv.get(key.PREFIX);
+  }
+}
