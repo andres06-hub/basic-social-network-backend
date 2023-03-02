008cc603a438a8156198e8c2a699ba827c600312
andres06-hub <andressierrarojas7@gmail.com>
2023-03-02 18:31:12 -0500

    ADD: entity user

--- src/providers/database/database.provider.ts
+++ src/providers/database/database.provider.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import * as path from 'path';
-// import * as appRootPath from 'app-root-path';
+import * as appRootPath from 'app-root-path';
 import { config } from 'dotenv';
 // import { User } from 'src/models/user.entity';
 import { Logger } from '@nestjs/common';
+import { User } from 'src/models/user.entity';
 
 config({ path: 'db.env' });
 
@@ -16,11 +17,18 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'sqlite',
         database: process.env.SQLITE_DB,
-        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        entities: [
+          // `${path.join(
+          //   appRootPath.path,
+          //   'src',
+          //   'models',
+          // )}/**/*.entity.{ts,js}`,
+          User,
+        ],
         synchronize: true,
       });
       const x = await dataSource.initialize();
-      logger.log('Connection DB');
+      logger.log('Connceted DB');
       return x;
     },
   },
