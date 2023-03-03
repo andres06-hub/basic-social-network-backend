d7ea6455d75832f7effed678e8c164266236f4bf
andres06-hub <andressierrarojas7@gmail.com>
2023-03-02 21:20:49 -0500

    MODIFIED

--- src/providers/database/database.module.ts
+++ src/providers/database/database.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.provider';
+import { Module, Post } from '@nestjs/common';
+import { dataSource, databaseProviders } from './database.provider';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/models/user.entity';
+import { config } from 'dotenv';
+
+config({ path: 'db.env' });
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.SQLITE_DB,
+      entities: [Post, User],
+      synchronize: true,
+    }),
+  ],
+  // providers: [...databaseProviders],
+  // exports: [...databaseProviders],
 })
 export class DatabaseModule {}
--- src/providers/database/database.provider.ts
+++ src/providers/database/database.provider.ts
@@ -1,35 +1,50 @@
 import { DataSource } from 'typeorm';
 import * as path from 'path';
-import * as appRootPath from 'app-root-path';
+// import * as appRootPath from 'app-root-path';
 import { config } from 'dotenv';
 // import { User } from 'src/models/user.entity';
 import { Logger } from '@nestjs/common';
 import { User } from 'src/models/user.entity';
+import { Post } from 'src/models/post.entity';
 
 config({ path: 'db.env' });
 
 const logger = new Logger();
 
 export const databaseProviders = [
+  // {
+  //   provide: 'DATA_SOURCE',
+  //   useFactory: async () => {
+  //     const dataSource = new DataSource({
+  //       type: 'sqlite',
+  //       database: process.env.SQLITE_DB,
+  //       entities: [
+  //         // `${path.join(
+  //         //   appRootPath.path,
+  //         //   'src',
+  //         //   'models',
+  //         // )}/**/*.entity.{ts,js}`,
+  //         Post,
+  //         User,
+  //       ],
+  //       synchronize: true,
+  //     });
+  //     const x = await dataSource.initialize();
+  //     logger.log('Connceted DB');
+  //     return x;
+  //   },
+  // },
   {
-    provide: 'DATA_SOURCE',
-    useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'sqlite',
-        database: process.env.SQLITE_DB,
-        entities: [
-          // `${path.join(
-          //   appRootPath.path,
-          //   'src',
-          //   'models',
-          // )}/**/*.entity.{ts,js}`,
-          User,
-        ],
-        synchronize: true,
-      });
-      const x = await dataSource.initialize();
-      logger.log('Connceted DB');
-      return x;
-    },
+    type: 'sqlite',
+    database: process.env.SQLITE_DB,
+    entities: [Post, User],
+    synchronize: true,
   },
 ];
+
+export const dataSource = {
+  type: 'sqlite',
+  database: process.env.SQLITE_DB,
+  entities: [Post, User],
+  synchronize: true,
+};
