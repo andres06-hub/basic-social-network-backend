cb29c6fae7b119c80d62a2d7f4a73f20ef7ea181
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 04:08:21 -0500

    ADD: files

--- src/common/dto/response.ts
+++ src/common/dto/response.ts
@@ -1,10 +1,10 @@
 export class Response {
-  constructor(status: number, message: string, data: object) {
+  constructor(status: number, message: string, data?: object) {
     this.status = status;
     this.message = message;
     this.data = data;
   }
   private status: number;
   private message: string;
-  private data: object;
+  private data?: object;
 }
