7c25462ea121345090af5db6a02d64a9ff919b65
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 00:13:35 -0500

    ADD: type Respose

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,7 @@ import {
 import { DataSignUpDto } from './dto/signUp.dto';
 import { UserService } from './user.service';
 import { User } from 'src/models/user.entity';
-import { DataPostDto } from '../post/dto/dataPost.dto';
+import { Response } from 'src/common/dto/response';
 
 @Controller('user')
 export class UserController {
@@ -20,6 +20,7 @@ export class UserController {
   @Post('signup')
   @HttpCode(HttpStatus.OK)
   async signUp(@Body() data: DataSignUpDto) {
+    //TODO: validate body empty
     const found: User | boolean | null = await this._userSrv.findOneByMail(
       data.email,
     );
@@ -31,11 +32,10 @@ export class UserController {
       throw new InternalServerErrorException(
         'ERROR: When searching for the user',
       );
+    this.logger.log('User not exist!');
     const result: User | boolean = await this._userSrv.createUser(data);
     if (!result) return;
-    this.logger.log('User Created!');
-    return {
-      message: 'User Created!',
-    };
+    this.logger.log('User Created!', User);
+    return new Response(200, 'User created!', result);
   }
 }
