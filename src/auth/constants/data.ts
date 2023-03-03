c0687d9f3e50a54788a752dea55f32238c8be71c
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 02:31:27 -0500

    CREATED: Interface for JWTPayload, and constants of key

--- src/auth/constants/data.ts
+++ src/auth/constants/data.ts
@@ -1,11 +1,7 @@
-// import { readFileSync } from 'fs';
-// import { join } from 'path';
 import { config } from 'dotenv';
 
-config({ path: '.secret.key' });
+config({ path: 'app.env' });
 
 export const JWTKEYS = {
-  //TODO: File Read for the key secret
-  secret: 'holaMundo',
-  // secret: readFileSync(join(process.cwd(), '.secret', 'sign.key')),
+  secret: process.env.JWT_SECRET,
 };
