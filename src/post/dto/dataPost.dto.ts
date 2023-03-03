73a6c2349537000937b90e1176c064d75deccc64
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 00:16:40 -0500

    ADD: fiels for validate

--- src/post/dto/dataPost.dto.ts
+++ src/post/dto/dataPost.dto.ts
@@ -1,10 +1,24 @@
 import { IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
 export class DataPostDto {
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: 'Title is required!',
+  })
   title: string;
 
-  @MinLength(50)
+  @MinLength(50, {
+    message: 'message is too Length',
+  })
   @MaxLength(255)
   message: string;
+
+  @IsNotEmpty({
+    message: 'Date is required!',
+  })
+  craeteAt: number;
+
+  @IsNotEmpty({
+    message: 'userId is required!',
+  })
+  userId: number;
 }
